da684fadb105cef9b51f56fcfc29537c1570bfef
Sherab-Z <codebodhi34@gmail.com>
2023-03-27 14:32:38 +0200

    Finish map, reduce, filter exercises

--- worksheet.js
+++ worksheet.js
@@ -31,21 +31,65 @@ const characters = [
 
 //***MAP***
 //1. Get array of all names
+const names = characters.map(character => {
+    return character.name;
+});
+console.log(names);
 //2. Get array of all heights
+const heights = characters.map(character => {
+    return character.height;
+});
+console.log(heights);
 //3. Get array of objects with just name and height properties
+const nameAndHeightObjs = characters.map(character => {
+    return {'name': character.name, 'height': character.height};
+});
+console.log(nameAndHeightObjs);
 //4. Get array of all first names
+const firstNamesArr = characters.map(character => {
+    return character.name.split(' ')[0];
+});
+console.log(firstNamesArr);
 
 //***REDUCE***
 //1. Get total mass of all characters
+const totalMass = characters.reduce((totalMass, character) => totalMass + character.mass, 0);
+console.log(totalMass);
+
 //2. Get total height of all characters
+const totalHeight = characters.reduce((totalHeight, character) => totalHeight + character.height, 0);
+console.log(totalHeight);
+
 //3. Get total number of characters by eye color
+const charactersByEyeColor = characters.reduce((acc, cur) => {
+    const color = cur.eye_color;
+    if(acc[color]) {
+        acc[color]++;
+    } else {
+        acc[color] = 1;
+    }
+    return acc;
+}, {});
+console.log(charactersByEyeColor);
+
 //4. Get total number of characters in all the character names
+const totalNameCharacters = characters.reduce((acc, cur) => acc + cur.name.length, 0
+);
+console.log(totalNameCharacters);
 
 //***FILTER***
 //1. Get characters with mass greater than 100
+const massOver100 = characters.filter(character => character.mass > 100);
+console.log(massOver100);
 //2. Get characters with height less than 200
+const heightUnder200 = characters.filter(character => character.height < 200);
+console.log(heightUnder200);
 //3. Get all male characters
+const maleCharacters = characters.filter(character => character.gender === 'male');
+console.log(maleCharacters);
 //4. Get all female characters
+const femaleCharacters = characters.filter(character => character.gender === 'female');
+console.log(femaleCharacters);
 
 //***SORT***
 //1. Sort by mass
